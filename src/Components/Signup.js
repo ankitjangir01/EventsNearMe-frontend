afc872aaf6472df488cc6303206dd89346f10991
Ankit Jangir <ankitjangirnice@gmail.com>
2022-08-03 19:18:11 +0530

    login and signup endpints working perfectly client side

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState({ email: "", password: "" })
+    const onChange = (eve) => {
+        setUser({ ...user, [eve.target.name]: eve.target.value });
+    }
+
+    const onClick = async () => {
+        const response = await fetch('http://localhost:5000/api/auth/login', {
+            method: 'post',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                'email': user.email,
+                'password': user.password
+            })
+        });
+        const json = await response.json();
+        if (json.success === true) {
+            toast.success("login success!", { autoClose: 2000 });
+            localStorage.setItem('authToken', json.authToken);
+            navigate(-1);
+        }
+        if(json.success === false){
+            toast.error("incorrect credentials", {autoClost: 2000})
+        }
+    }
+
     return (
         <div>
             <Navbar />
             <div className="signup-page text-center">
                 <h1 className='text-align-right pt-4 text-dark'><strong>Create New Account or Sign In</strong></h1>
-                <hr/>
+                <hr />
                 <div className="d-flex justify-content-center">
                     <div className="add-event-form mx-4 my-4 bg-light">
                         <div className="mx-4 my-4 bg-light">
@@ -32,10 +62,10 @@ const Signup = () => {
 
                     <div className="add-event-form mx-4 my-4 bg-light">
                         <div className="mx-4 my-4 bg-light">
-                            <form className="form-control p-4" target='' method='POST' action="http://localhost:5000/api/auth/login" encType='multipart/form-data' >
-                                <input type="email" className="form-control mr-2 my-2" placeholder="Your Email" name='email' />
-                                <input type="password" className="form-control mr-2 my-2" placeholder="Enter password" name='password' />
-                                <button type="submit" className="btn btn-secondary custom-btn btn-block mt-4">Sign In</button>
+                            <form className="form-control p-4" method='POST' encType='multipart/form-data' >
+                                <input type="email" className="form-control mr-2 my-2" placeholder="Your Email" name='email' onChange={onChange} />
+                                <input type="password" className="form-control mr-2 my-2" placeholder="Enter password" name='password' onChange={onChange} />
+                                <button type="button" onClick={onClick} className="btn btn-secondary custom-btn btn-block mt-4">Sign In</button>
                             </form>
                         </div>
                     </div>
